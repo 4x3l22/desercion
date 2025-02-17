4872de266696ad99642b541c5f746486fb366019
jhonnCorredor <jhoncorredormedina29@gmail.com>
2025-02-17 16:31:26 -0500

    Arreglo consulta login

--- appdesercion/Entity/Dao/Usuario_dao.py
+++ appdesercion/Entity/Dao/Usuario_dao.py
@@ -13,7 +13,7 @@ class UsuarioDAO(BaseDAO):
             cursor.execute("""
                 SELECT U.Id AS usuario_id, 
                 U.contrasena, 
-                ur.Id AS rol_id
+                ur.rol_id_id AS rol_id
                 FROM sena.Usuario AS U
                 LEFT JOIN sena.UsuarioRol AS ur ON ur.usuario_id_id = U.Id
                 WHERE U.correo = %s;
