40fbe9ed9b07efb54256f9e020a54bb89c0095e1
Alejandro <ar3827195@gmail.com>
2025-02-19 08:58:32 -0500

    nuevo cambio

--- appdesercion/Entity/Dao/aprendiz_dao.py
+++ appdesercion/Entity/Dao/aprendiz_dao.py
@@ -4,3 +4,7 @@ from appdesercion.models import Aprendiz
 
 class AprendizDAO(BaseDAO):
     model=Aprendiz
+
+    @staticmethod
+    def consultar_por_documento(documento):
+        return Aprendiz.objects.select_related('tipoDocumento').filter(documento=documento).first()
